ed4feca301b34a0d34f7fb8db3d425946df55421
FA-010A FAZZ <35626594+June12138@users.noreply.github.com>
2025-11-21 18:30:50 +0800

    AIToken

---
+++ Source/FazzUETools/Public/AIToken/AITokenStruct.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AITokenStruct.generated.h"
+USTRUCT(BlueprintType)
+struct FAITokenStruct
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Token")
+	FName Key;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Token")
+	TArray<AActor*> Holders;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Token")
+	int MaxHolders;
+};
